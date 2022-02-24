7e4a5768bb8e525e486d721c58b28341a46a94f9
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-02-24 10:22:24 +0700

    bai thi

---
+++ EmployeeClient/EmployeeClient/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeClient.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ServiceReference1.Service1Client service1Client = new ServiceReference1.Service1Client();
+        public ActionResult Index()
+        {
+            var listProduct = service1Client.GetList();
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(Employee employee)
+        {
+            service1Client.Save(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmployeeService/EmployeeService/Entity/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace EmployeeService.Entity
+{
+    [DataContract]
+    public class Employee
+    {
+        [Key]
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string EmployeeName { get; set; }
+        [DataMember]
+        public string Salary { get; set; }
+        [DataMember]
+        public string Department { get; set; }
+    }
+}
